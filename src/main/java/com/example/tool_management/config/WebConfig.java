f7cfe174f664c209f24579826550ddf6262ccae6
HIroyuki O <ohtsuka.saijou@gmail.com>
2025-03-23 20:22:59 +0900

    Fix: Update CORS IP to current EC2 address

--- src/main/java/com/example/tool_management/config/WebConfig.java
+++ src/main/java/com/example/tool_management/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://54.87.3.97:3000")  // フロントエンドのURL
+            .allowedOrigins("http://54.172.221.49:3000")  // フロントエンドのURL
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowCredentials(true);
       }
